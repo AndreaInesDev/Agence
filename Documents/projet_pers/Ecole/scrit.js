5a9e86d4eb406408172bc952bd09deabaa895bf3
Andrea Ines <andreaines50@gmail.com>
2025-11-28 11:33:44 +0100

    Premier commit du projet Ecole

---
+++ Documents/projet_pers/Ecole/scrit.js
@@ -0,0 +1,10 @@
+let navigation = document.getElementById("barreMenu");
+
+function hideMenus() {
+    navigation.style.right = "-200px";
+}
+
+function openMenus() {
+    navigation.style.right = "0";
+}
+
